048de19ade8cd5182366351066b42039a38c3506
xo-Natsumi-ox <shevchukdiana1@gmail.com>
2020-01-29 16:53:06 +0200

    fixed code according to convention

--- lab1/src/ua/lviv/iot/firstlab/Stadium.java
+++ lab1/src/ua/lviv/iot/firstlab/Stadium.java
@@ -1,42 +1,54 @@
 package ua.lviv.iot.firstlab;
 
 public class Stadium {
-    private static int number_of_stadiums;
-    private int number_of_viewers;
+    private static int numberOfStadiums;
+    private int numberOfViewers;
     private String name;
-    private int power_of_lighting_inlux;
-    private int opening_year;
+    private int powerOfLightingInLux;
+    private int openingYear;
     private String town;
-    protected boolean working_now;
+    protected boolean workingNow;
     protected String country;
 
 
-    Stadium() {
+    public Stadium() {
 
     }
 
-    public Stadium(int Number_of_stadiums, int number_of_viewers, String name, int power_of_lighting_inlux, int opening_year, String town, boolean working_now, String country) {
-        this.number_of_viewers = number_of_viewers;
+    public void resetValues(int numberOfStadium, int numberOfViewers, String name, int powerOfLightingInLux, int openingYear, String town, boolean workingNow, String country) {
+        this.numberOfViewers = numberOfViewers;
         this.name = name;
-        this.power_of_lighting_inlux = power_of_lighting_inlux;
-        this.opening_year = opening_year;
+        this.powerOfLightingInLux = powerOfLightingInLux;
+        this.openingYear = openingYear;
         this.town = town;
-        this.working_now = working_now;
+        this.workingNow = workingNow;
         this.country = country;
-        number_of_stadiums = Number_of_stadiums;
-
+        numberOfStadiums = numberOfStadium;
     }
 
-    public Stadium(int Number_of_stadiums, int number_of_viewers, String name, int power_of_lighting_inlux, int opening_year) {
-        this(Number_of_stadiums, number_of_viewers, name, power_of_lighting_inlux, opening_year, "unknown", false, "Ukraine");
+    public Stadium(int numberOfStadium, int numberOfViewers, String name, int powerOfLightingInLux, int openingYear, String town, boolean workingNow, String country) {
+        this.resetValues(numberOfStadium, numberOfViewers, name, powerOfLightingInLux, openingYear, town, workingNow, country);
     }
 
-    public int getNumber_of_viewers() {
-        return number_of_viewers;
+    public Stadium(int numberOfStadium, int numberOfViewers, String name, int powerOfLightingInLux, int openingYear) {
+        this.resetValues(numberOfStadium, numberOfViewers, name, powerOfLightingInLux, openingYear, town, workingNow, country);
     }
 
-    public void setNumber_of_viewers(int number_of_viewers) {
-        this.number_of_viewers = number_of_viewers;
+
+    public static int getNumberOfStadiums() {
+        return numberOfStadiums;
+    }
+
+    public static void setNumberOfStadiums(int numberOfStadiums) {
+        Stadium.numberOfStadiums = numberOfStadiums;
+    }
+
+    public int getNumberOfViewers() {
+        return numberOfViewers;
+    }
+
+    public void setNumberOfViewers(int numberOfViewers) {
+        this.numberOfViewers = numberOfViewers;
     }
 
     public String getName() {
@@ -47,20 +59,20 @@ public class Stadium {
         this.name = name;
     }
 
-    public int getPower_of_lighting_inlux() {
-        return power_of_lighting_inlux;
+    public int getPowerOfLightingInLux() {
+        return powerOfLightingInLux;
     }
 
-    public void setPower_of_lighting_inlux(int power_of_lighting_inlux) {
-        this.power_of_lighting_inlux = power_of_lighting_inlux;
+    public void setPowerOfLightingInLux(int powerOfLightingInLux) {
+        this.powerOfLightingInLux = powerOfLightingInLux;
     }
 
-    public int getOpening_year() {
-        return opening_year;
+    public int getOpeningYear() {
+        return openingYear;
     }
 
-    public void setOpening_year(int opening_year) {
-        this.opening_year = opening_year;
+    public void setOpeningYear(int openingYear) {
+        this.openingYear = openingYear;
     }
 
     public String getTown() {
@@ -71,20 +83,12 @@ public class Stadium {
         this.town = town;
     }
 
-    public static int getNumber_of_stadiums() {
-        return number_of_stadiums;
+    public boolean isWorkingNow() {
+        return workingNow;
     }
 
-    public static void setNumber_of_stadiums(int number_of_stadiums) {
-        Stadium.number_of_stadiums = number_of_stadiums;
-    }
-
-    public boolean isWorking_now() {
-        return working_now;
-    }
-
-    public void setWorking_now(boolean working_now) {
-        this.working_now = working_now;
+    public void setWorkingNow(boolean workingNow) {
+        this.workingNow = workingNow;
     }
 
     public String getCountry() {
@@ -98,41 +102,30 @@ public class Stadium {
     @Override
     public String toString() {
         return "Stadium{" +
-                "number_of_stadiums = " + number_of_stadiums +
-                ", number_of_viewers = " + number_of_viewers +
+                "numberOfStadiums = " + numberOfStadiums +
+                ", numberOfViewers = " + numberOfViewers +
                 ", name = '" + name + '\'' +
-                ", power_of_lighting_inlux = " + power_of_lighting_inlux + " lux" +
-                ", opening_year = " + opening_year +
+                ", powerOfLightingInLux = " + powerOfLightingInLux +
+                ", openingYear = " + openingYear +
                 ", town = '" + town + '\'' +
-                ", working_now = " + working_now +
+                ", workingNow = " + workingNow +
                 ", country = '" + country + '\'' +
                 '}';
     }
 
-    public void resetValues(int Number_of_stadiums, int number_of_viewers, String name, int power_of_lighting_inlux, int opening_year, String town, boolean working_now, String country) {
-        this.number_of_viewers = number_of_viewers;
-        this.name = name;
-        this.power_of_lighting_inlux = power_of_lighting_inlux;
-        this.opening_year = opening_year;
-        this.town = town;
-        this.working_now = working_now;
-        this.country = country;
-        number_of_stadiums = Number_of_stadiums;
+    void printNumberOfStadiums() {
+        System.out.println("static numberOfStadiums = " + numberOfStadiums);
     }
 
-    void printNumber_of_stadiums() {
-        System.out.println("static number_of_stadiums = " + number_of_stadiums);
+    static void printNumberOfViewers(Stadium stadiumname) {
+        System.out.println("numberOfViewers " + stadiumname.name + " = " + stadiumname.numberOfViewers);
     }
 
-    static void printNumber_of_viewers(Stadium stadiumname) {
-        System.out.println("number_of_viewers " + stadiumname.name + " = " + stadiumname.number_of_viewers);
+    static void printOpeningYear(Stadium stadiumname) {
+        System.out.println("openingYear " + stadiumname.name + " = " + stadiumname.openingYear);
     }
 
-    static void printOpening_year(Stadium stadiumname) {
-        System.out.println("opening_year " + stadiumname.name + " = " + stadiumname.opening_year);
-    }
-
-    static void printPower_of_lighting_inlux(Stadium stadiumname) {
-        System.out.println("power_of_lighting_inlux in " + stadiumname.name + " = " + stadiumname.power_of_lighting_inlux);
+    static void printPowerOfLightingInLux(Stadium stadiumname) {
+        System.out.println("powerOfLightingInLux in " + stadiumname.name + " = " + stadiumname.powerOfLightingInLux);
     }
 }
--- lab1/src/ua/lviv/iot/firstlab/StadiumCreator.java
+++ lab1/src/ua/lviv/iot/firstlab/StadiumCreator.java
@@ -2,14 +2,14 @@ package ua.lviv.iot.firstlab;
 
 import static ua.lviv.iot.firstlab.Stadium.*;
 
-public class StadiumInfo {
+public class StadiumCreator {
     public static void main(String[] args) {
         System.out.println();
         System.out.println("├┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┤ STADIUM OBJECTS ├┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┤");
-        Stadium NSK = new Stadium(1, 70050, "Olympic Stadium", 500, 1923, "Kyiv", true, "Ukraine");
+        Stadium nsk = new Stadium(1, 70050, "Olympic Stadium", 500, 1923, "Kyiv", true, "Ukraine");
         Stadium dynamo = new Stadium(2, 16873, "Dynamo", 1200, 1934);
         Stadium arena = new Stadium();
-        System.out.println(NSK);
+        System.out.println(nsk);
         System.out.println(dynamo);
         System.out.println(arena);
         System.out.println("├┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┤ RESET VALUES ├┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┤");
@@ -18,25 +18,22 @@ public class StadiumInfo {
         System.out.println(arena);
         System.out.println();
         System.out.println("├┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┤ STATIC METHODS AND FIELD ├┬┴┬┴┬┴┬┴┬┴┬┴┬┴┤ಠ‿ಠ)├┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┤");
-        dynamo.printNumber_of_stadiums();
-        printNumber_of_viewers(NSK);
-        printOpening_year(arena);
-        printPower_of_lighting_inlux(dynamo);
+        dynamo.printNumberOfStadiums();
+        printNumberOfViewers(nsk);
+        printOpeningYear(arena);
+        printPowerOfLightingInLux(dynamo);
         System.out.println();
         System.out.println("├┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┤ 14 STEP ├┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┤");
-        //Stadium abroad[];
-        //Stadium random[];
-        int chisloforcikly = 0;
-        Stadium[] random = new Stadium[4];
+        int stadiumIterator = 0;
+        Stadium[] stadiums = new Stadium[4];
         do {
-            //int forViewersAndLighting = chisloforcikly;
-            random[chisloforcikly] = new Stadium(getNumber_of_stadiums() + 1, (chisloforcikly + 1) * 10000, "unknown", chisloforcikly * 400, 2020);
-            chisloforcikly++;
+            stadiums[stadiumIterator] = new Stadium(getNumberOfStadiums() + 1, (stadiumIterator + 1) * 10000, "unknown", stadiumIterator * 400, 2020);
+            stadiumIterator++;
         }
-        while (chisloforcikly < 4);
+        while (stadiumIterator < 4);
 
-        for (Stadium chisloforrandom : random) {
-            System.out.println(chisloforrandom);
+        for (Stadium arrayIterator : stadiums) {
+            System.out.println(arrayIterator);
         }
     }
 }
