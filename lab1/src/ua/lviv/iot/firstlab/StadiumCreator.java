a47780d168d957564105609da3774ef0357daba9
xo-Natsumi-ox <53785210+xo-Natsumi-ox@users.noreply.github.com>
2020-02-16 20:56:32 +0200

    Update StadiumCreator.java

--- lab1/src/ua/lviv/iot/firstlab/StadiumCreator.java
+++ lab1/src/ua/lviv/iot/firstlab/StadiumCreator.java
@@ -4,7 +4,6 @@ import static ua.lviv.iot.firstlab.Stadium.*;
 
 public class StadiumCreator {
     public static void main(String[] args) {
-        System.out.println();
         System.out.println("├┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┤ STADIUM OBJECTS ├┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┤");
         Stadium nsk = new Stadium(1, 70050, "Olympic Stadium", 500, 1923, "Kyiv", true, "Ukraine");
         Stadium dynamo = new Stadium(2, 16873, "Dynamo", 1200, 1934);
@@ -13,16 +12,13 @@ public class StadiumCreator {
         System.out.println(dynamo);
         System.out.println(arena);
         System.out.println("├┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┤ RESET VALUES ├┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┤");
-        System.out.println();
         arena.resetValues(3, 34951, "Arena Lviv", 2400, 2011, "Lviv", true, "Ukraine");
         System.out.println(arena);
-        System.out.println();
         System.out.println("├┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┤ STATIC METHODS AND FIELD ├┬┴┬┴┬┴┬┴┬┴┬┴┬┴┤ಠ‿ಠ)├┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┤");
         dynamo.printNumberOfStadiums();
         printNumberOfViewers(nsk);
         printOpeningYear(arena);
         printPowerOfLightingInLux(dynamo);
-        System.out.println();
         System.out.println("├┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┤ 14 STEP ├┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┬┴┤");
         int stadiumIterator = 0;
         Stadium[] stadiums = new Stadium[4];
